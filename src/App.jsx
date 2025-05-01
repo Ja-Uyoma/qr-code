f6e735ee3d7cc10028650c6545e5894e13b89748
Jimmy Givans <jauyoma.dev@gmail.com>
2025-05-01 13:07:30 +0300

    Wrap the text in the page in a section element

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ export default function App() {
       <figure>
         <img src={qrcode} alt="A QR Code that links to frontendmentor.io" />
       </figure>
-      <h2>Improve your front-end skills by building projects</h2>
-      <p>
-        Scan the QR code to visit Frontend Mentor and take your coding skills to
-        the next level
-      </p>
+      <section>
+        <h2>Improve your front-end skills by building projects</h2>
+        <p>
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </section>
     </main>
   );
 }
