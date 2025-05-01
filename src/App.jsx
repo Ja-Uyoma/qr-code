bc77a6c7a87509e04c8a1eca1c1b7ec95de657ba
Jimmy Givans <jauyoma.dev@gmail.com>
2025-05-01 19:25:42 +0300

    Make the h2 a h1

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
         <img src={qrcode} alt="A QR Code that links to frontendmentor.io" />
       </figure>
       <section>
-        <h2>Improve your front-end skills by building projects</h2>
+        <h1>Improve your front-end skills by building projects</h1>
         <p>
           Scan the QR code to visit Frontend Mentor and take your coding skills
           to the next level
